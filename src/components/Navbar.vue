3396af07ff88f19dbae71fc1415dd4c8b606f18d
ahmadRamadhanJuara <ahmadoneperson@gmail.com>
2022-10-10 05:31:24 +0700

    chore: setup fontawesome v5

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center py-2">
+    <div class="w-10/12 flex justify-between">
+      <div class="mt-5 flex gap-10">
+        <img
+          class="w-28"
+          src="https://www.noice.id/wp-content/themes/zerif-lite/images/logo.svg"
+          alt=""
+        />
+        <ul class="text-white flex mt-3 gap-8">
+          <li
+            class="font-semibold text-[14px] rounded-md bg-[#202020] text-[#FAD810]"
+          >
+            Beranda
+          </li>
+          <li class="font-semibold text-[14px]">Tentang Noice</li>
+          <li class="font-semibold text-[14px]">Info Terbaru</li>
+          <li class="font-semibold text-[14px]">Noicemaker Studio</li>
+          <li class="font-semibold text-[14px]">Iklan</li>
+        </ul>
+      </div>
+      <button
+        class="text-[.8rem] uppercase mt-6 py-2 px-5 rounded-md bg-white font-bold"
+      >
+        putar konten
+      </button>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlay);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
